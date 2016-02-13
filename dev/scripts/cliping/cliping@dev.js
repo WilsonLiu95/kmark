5a53b497eced0a7c5589b465fcbe1556ae6203f8
WilsonLiu <wilsonliuxyz@gmail.com>
2016-02-13 18:44:31 +0800

    改图片尾base64jpeg手机无法显示

---
+++ dev/scripts/cliping/cliping@dev.js
@@ -0,0 +1,55 @@
+
+;(function(__context){
+    var module = {
+        id : "912f8dff31ecfe86e592860e45a7d97f" ,
+        filename : "cliping.string" ,
+        exports : {}
+    };
+    if( !__context.____MODULES ) { __context.____MODULES = {}; }
+    var r = (function( exports , module , global ){
+
+    if (typeof window.QTMPL === "undefined") window.QTMPL = {};
+window.QTMPL["cliping"] = "<!--左侧展示栏-->\n<nav class=\"nav-side\">\n    <ul>\n        <li class=\"menu-item\" ng-click=\"select(-1)\" ng-if=\"params.notes\">显示全部</li>\n        <li class=\"menu-item\" ng-repeat=\"book in params.notes\" ng-click=\"select($index)\">\n            {{book.bookName}}\n        </li>\n    </ul>\n</nav>\n<!--右侧主体部分-->\n<div class=\"main-warp\">\n    <!--功能按钮部分-->\n    <div class=\"tool-section\">\n        <label class=\"file-label\">选择文件\n            <input type=\"file\" ng-model=\"filePath\" file-reader=\"result\">\n        </label>\n        <span>欢迎使用kindle标记伴侣</span>\n    </div>\n    <!--展示部分-->\n    <div>\n        <article class=\"book-content\" ng-repeat=\"note in params.notes\" ng-if=\"$index===params.show||params.show===-1\">\n            <!--当show为-1时，允许用户显示全部-->\n            <div class=\"book-header\">\n                <h1 class=\"book-name\">\n                    {{note.bookName}}\n                </h1>\n                <span class=\"book-author\">{{note.author}}</span>\n            </div>\n            <li ng-repeat=\"fragment in note.fragments track by $index\">\n                {{fragment}}\n            </li>\n        </article>\n    </div>\n</div>\n";
+if (typeof module !== "undefined") module.exports = window.QTMPL["cliping"];
+
+    })( module.exports , module , __context );
+    __context.____MODULES[ "912f8dff31ecfe86e592860e45a7d97f" ] = module.exports;
+})(this);
+
+
+;(function(__context){
+    var module = {
+        id : "9fe8c98ea96b3e4c8ccea76acbc0e666" ,
+        filename : "cliping.js" ,
+        exports : {}
+    };
+    if( !__context.____MODULES ) { __context.____MODULES = {}; }
+    var r = (function( exports , module , global ){
+
+    var html = __context.____MODULES['912f8dff31ecfe86e592860e45a7d97f'];
+angular.setView(html);
+
+app.controller("main", ["$scope", "deal", function ($scope, deal) {
+    $scope.params = {
+        show: -1 //当前展示的书名序号
+    };
+    $scope.$watch('result', function () {
+        if ($scope.result) {
+            $scope.params.notes = deal.pipeline($scope.result);
+            console.log($scope);
+            if (!$scope.params.notes[0]){
+                alert("请检查您选取的文件是否符合kindle标记的标准格式")
+            }
+        }
+    });
+    $scope.select = function (index) {
+        $scope.params.show = index;
+    };
+}]);
+
+
+
+
+    })( module.exports , module , __context );
+    __context.____MODULES[ "9fe8c98ea96b3e4c8ccea76acbc0e666" ] = module.exports;
+})(this);
