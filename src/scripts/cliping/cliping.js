eede0571e6a5e64084ad3193ca496fa7e7ba0058
Wilson <wilsonliuxyz@gmail.com>
2016-07-03 17:14:13 +0800

    更迭

--- src/scripts/cliping/cliping.js
+++ src/scripts/cliping/cliping.js
@@ -1,6 +1,5 @@
 var html = require("html/cliping");
 angular.setView(html);
-
 app.controller("main", ["$scope", "deal", function ($scope, deal) {
     $scope.params = {
         show: -1 //当前展示的书名序号
@@ -8,7 +7,6 @@ app.controller("main", ["$scope", "deal", function ($scope, deal) {
     $scope.$watch('result', function () {
         if ($scope.result) {
             $scope.params.notes = deal.pipeline($scope.result);
-            console.log($scope);
             if (!$scope.params.notes[0]){
                 alert("请检查您选取的文件是否符合kindle标记的标准格式");
             }
---
+++ src/scripts/intro/intro.js
@@ -0,0 +1,6 @@
+var html = require("html/intro");
+angular.setView(html);
+
+app.controller("main", ["$scope", function ($scope, deal) {
+
+}]);
