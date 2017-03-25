fad5383e061b6b76d8404f5e75b5f8eac0ca64a8
Wilson <wilsonliuxyz@gmail.com>
2017-03-25 13:41:05 +0800

    更新

--- htdocs/kmark/BackEnd/app/Http/Controllers/test.php
+++ htdocs/kmark/BackEnd/app/Http/Controllers/test.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class test extends Controller
+class Test extends Controller
 {
     public function getIndex(){
         return '生效';
