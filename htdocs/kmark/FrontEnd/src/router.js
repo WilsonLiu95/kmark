a611f4a45d8799bbc966093eac2e39b93eaedfb2
Wilson <wilsonliuxyz@gmail.com>
2017-04-06 14:59:55 +0800

    代码路由分割

--- htdocs/kmark/FrontEnd/src/router.js
+++ htdocs/kmark/FrontEnd/src/router.js
@@ -1,10 +1,38 @@
 export default {
   routes: [
-    // ========================================PC管理系统========================================
-    { path: '/', redirect: '/home' },
-    { name: 'home', meta: '首页', path: '/home', component: require('./views/home.vue') },
-    { name: 'static', meta: '统计', path: '/static', component: require('./views/static.vue') },
-    { name: 'about', meta: '关于', path: '/about', component: require('./views/about.vue') }
-    // { path: '*', component: require('_views/404.vue') }
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      name: 'home',
+      meta: '首页',
+      path: '/home',
+      component: resolve => {
+        require.ensure([], (require) => {
+          resolve(require('./views/home.vue'))
+        })
+      }
+    },
+    {
+      name: 'static',
+      meta: '统计',
+      path: '/static',
+      component: resolve => {
+        require.ensure([], (require) => {
+          resolve(require('./views/static.vue'))
+        })
+      }
+    },
+    {
+      name: 'about',
+      meta: '关于',
+      path: '/about',
+      component: resolve => {
+        require.ensure([], (require) => {
+          resolve(require('./views/about.vue'))
+        })
+      }
+    }
   ]
 }
